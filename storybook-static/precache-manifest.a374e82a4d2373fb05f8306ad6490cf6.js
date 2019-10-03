3e1315ed6225a2d422e1c9741a53bae425c49e40
Ashraf Anwar <ashraf.anwar-apandi@wsp.com>
2019-10-03 15:47:48 +0100

    Add initial storybook deploy pack

---
+++ storybook-static/precache-manifest.a374e82a4d2373fb05f8306ad6490cf6.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e74e80e1f323d5ec46a9cdb78a35b7d",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "89ceda8611c5ef69aeca",
+    "url": "main.css"
+  },
+  {
+    "url": "main.d663c4684ce28effd89f.bundle.js"
+  },
+  {
+    "url": "runtime~main.d663c4684ce28effd89f.bundle.js"
+  },
+  {
+    "url": "vendors~main.d663c4684ce28effd89f.bundle.js"
+  }
+]);
